c9e6376feb15935add89421da17a1bc3c663a88c
Cynthia Powell <aruchty@value.net>
2019-11-17 19:40:54 +0000

    fine tune app.js spacing

--- src/App.js
+++ src/App.js
@@ -19,111 +19,110 @@ class App extends Component {
     };
   }
 
-toggleNote = () => {
-  this.setState({
-    showNote: ! this.state.showNote,
-    note: {}
-  });
-}
-
-getNotes = () => {
-  axios.get(urlFor('notes'))
-  .then((res) => this.setState({ notes: res.data }) )
-  .catch((err) => console.log(err.response.data) );
+  toggleNote = () => {
+    this.setState({
+      showNote: ! this.state.showNote,
+        note: {}
+      });
   }
 
-getNote = (id) => {
-  axios.get(urlFor(`notes/${id}`))
-  .then((res) => this.setState({ note: res.data, showNote: true }) )
-  .catch((err) => console.log(err.response.data) );
+  getNotes = () => {
+    axios.get(urlFor('notes'))
+    .then((res) => this.setState({ notes: res.data }) )
+    .catch((err) => console.log(err.response.data) );
   }
 
-performSubmissionRequest = (data, id) => {
-  if (id) {
-    return axios.patch(urlFor(`notes/${id}`), data);
-  } else {
-    return axios.post(urlFor('notes'), data);
+  getNote = (id) => {
+    axios.get(urlFor(`notes/${id}`))
+    .then((res) => this.setState({ note: res.data, showNote: true }) )
+    .catch((err) => console.log(err.response.data) );
   }
-}
 
-submitNote = (data, id) => {
-  this.performSubmissionRequest(data, id)
-  .then((res) => this.setState({ showNote: false }) )
-  .catch((err) => {
-    const { errors } = err.response.data;
-      if (errors.content) {
-        this.setState({ error: "Missing Note Content!" });
-      } else if (errors.title) {
-        this.setState({ error: "Missing Note Title!" });
+  performSubmissionRequest = (data, id) => {
+    if (id) {
+      return axios.patch(urlFor(`notes/${id}`), data);
+    } else {
+      return axios.post(urlFor('notes'), data);
+    }
+  }
+
+  submitNote = (data, id) => {
+    this.performSubmissionRequest(data, id)
+    .then((res) => this.setState({ showNote: false }) )
+    .catch((err) => {
+      const { errors } = err.response.data;
+        if (errors.content) {
+          this.setState({ error: "Missing Note Content!" });
+        } else if (errors.title) {
+          this.setState({ error: "Missing Note Title!" });
+        }
+    });
+  }
+
+  deleteNote = (id) => {
+    const newNotesState = this.state.notes.filter((note) => note.id !== id );
+    axios.delete(urlFor(`notes/${id}`))
+    .then((res) => this.setState({ notes: newNotesState }))
+    .catch((err) => console.log(err.response.data) );
+  }
+
+  showTagForm = () => {
+    this.setState({ newTag: true });
+  }
+
+  closeTagForm = () => {
+    this.setState({ newTag: false });
+  }
+
+  submitTag = (data, noteId) => {
+    axios.post(urlFor(`notes/${noteId}/tags`), data)
+    .then((res) => this.getNote(noteId) )
+    .catch((err) => {
+      const { errors } = err.response.data;
+      if (errors.name) {
+        this.setState({ error: "Missing Tag Name!"});
       }
     });
   }
 
-deleteNote = (id) => {
-  const newNotesState = this.state.notes.filter((note) => note.id !== id );
-  axios.delete(urlFor(`notes/${id}`))
-  .then((res) => this.setState({ notes: newNotesState }))
-  .catch((err) => console.log(err.response.data) );
+  deleteTag  = (noteId, id) => {
+    axios.delete(urlFor(`/tags/${id}`))
+    .then((res) => this.getNote(noteId) )
+    .catch((err) => console.log(err.response.data) );
   }
 
-showTagForm = () => {
-  this.setState({ newTag: true });
-  }
-
-closeTagForm = () => {
-  this.setState({ newTag: false });
-  }
-
-submitTag = (data, noteId) => {
-  axios.post(urlFor(`notes/${noteId}/tags`), data)
-  .then((res) => this.getNote(noteId) )
-  .catch((err) => {
-    const { errors } = err.response.data;
-    if (errors.name) {
-      this.setState({ error: "Missing Tag Name!"});
-    }
-  });
-}
-
-deleteTag  = (noteId, id) => {
-  axios.delete(urlFor(`/tags/${id}`))
-  .then((res) => this.getNote(noteId) )
-  .catch((err) => console.log(err.response.data) );
-}
-
-resetError = () => {
+  resetError = () => {
     this.setState({ error: '' });
-}
+  }
 
-render() {
-  const { showNote, notes, note, newTag, error } = this.state;
+  render() {
+    const { showNote, notes, note, newTag, error } = this.state;
 
-  return (
-    <div className="App">
-      <Nav toggleNote={this.toggleNote} showNote={showNote} />
-      {error && <Flash error={error} resetError={this.resetError} />}
-      {showNote ?
-        <Note
-          note={note}
-          submitNote={this.submitNote}
-          newTag={newTag}
-          showTagForm={this.showTagForm}
-          closeTagForm={this.closeTagForm}
-          submitTag={this.submitTag}
-          deleteTag={this.deleteTag}
-        />
-        :
-        <List
-          getNotes={this.getNotes}
-          notes={notes}
-          getNote={this.getNote}
-          deleteNote={this.deleteNote}
-        />
-      }
-
-    </div>
-  );
-}
+    return (
+      <div className="App">
+        <Nav toggleNote={this.toggleNote} showNote={showNote} />
+        {error && <Flash error={error} resetError={this.resetError} />}
+        {showNote ?
+          <Note
+            note={note}
+            submitNote={this.submitNote}
+            newTag={newTag}
+            showTagForm={this.showTagForm}
+            closeTagForm={this.closeTagForm}
+            submitTag={this.submitTag}
+            deleteTag={this.deleteTag}
+          />
+          :
+          <List
+            getNotes={this.getNotes}
+            notes={notes}
+            getNote={this.getNote}
+            deleteNote={this.deleteNote}
+          />
+        }
+      </div>
+    );
+  }
 }
 
 export default App;
