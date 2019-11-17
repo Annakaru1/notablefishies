ec04a0b921ab06949d85f223ffca95ae52f267a4
Cynthia Powell <aruchty@value.net>
2019-11-17 01:11:47 +0000

    form submission

--- src/App.js
+++ src/App.js
@@ -61,6 +61,10 @@ showTagForm = () => {
   this.setState({ newTag: true });
   }
 
+closeTagForm = () => {
+  this.setState({ newTag: false });
+  }
+
 render() {
   const { showNote, notes, note, newTag } = this.state;
 
@@ -71,8 +75,9 @@ render() {
         <Note
           note={note}
           submitNote={this.submitNote}
-          showTagForm={this.showTagForm}
           newTag={newTag}
+          showTagForm={this.showTagForm}
+          closeTagForm={this.closeTagForm}
         />
         :
         <List
